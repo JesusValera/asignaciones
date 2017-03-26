c1d2b3641acd9a4883d1268afb61c5b6b2430a85
jesus <jesus1902@outlook.com>
2017-03-26 22:02:44 +0200

    Video 6.- First steps with ORM Doctrine.

--- src/JVRUserBundle/Controller/UserController.php
+++ src/JVRUserBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace JVRUserBundle\Controller;
 
+use JVRUserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,10 +10,35 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return new Response("Bienvenidos a mi modulo de usuarios");
+        // Obtener el manager que hace de puente con la base de datos.
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository("JVRUserBundle:User")->findAll();
+
+        // Mostrar datos.
+        $res = 'Lista de usuarios: <br/>';
+        /** @var User $user */
+        foreach ($users as $user)
+        {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br/>';
+        }
+
+        return new Response($res);
     }
 
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository("JVRUserBundle:User");
 
+        /** @var User $user */
+        $user = $repository->find($id);
 
+        // You can also write whatever attribute to find for it. EX:
+        //$user = $repository->findOneById($id);
+        //$user = $repository->findOneByUsername($name);
+        //$user = $repository->findOneByPassword($password);
+
+        return new Response('Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br/>');
+    }
 
 }
